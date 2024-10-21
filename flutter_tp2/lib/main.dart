1842b0789527fb4301a61978e7727ac8f5f05317
Nour Elhouda Moussa <moussanouralhouda@gmail.com>
2024-10-21 23:30:05 +0200

    update tp 2

--- flutter_tp2/lib/main.dart
+++ flutter_tp2/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,10 +11,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Tp2",
-      theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
-          useMaterial3: true),
-      home: MyHomePage(),
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),useMaterial3: true),
+      home: const MyHomePage(),
     );
   }
 }
@@ -28,10 +26,10 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Home Page"),
+          title: const Center(child:Text("Home Page")),
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      body:Center(child: Text("Hello Word")),
+        ),
+      body:const Center(child: Text("Hello Word",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),)),
     );
   }
 }
